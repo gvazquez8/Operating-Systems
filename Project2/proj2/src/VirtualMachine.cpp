e262c4e1cdc96cec04a259ad01338b1a662fce0e
gvazquez8 <gabvazquez@ucdavis.edu>
2020-05-04 12:12:27 -0700

    editing

--- Project2/proj2/src/VirtualMachine.cpp
+++ Project2/proj2/src/VirtualMachine.cpp
@@ -156,6 +156,8 @@ extern "C" {
 	void timerCallback(void* calldata) {
 		totalTickCount++;
 		for (unsigned int i = 0; i < sleepingThreads.size(); i++) {
+			std::cout << "Sleeping Thread id: " << threadHolder[sleepingThreads[i]].id;
+			std::cout << " ticks Remaining: " << threadHolder[sleepingThreads[i]].sleepCountdown;
 			if (threadHolder[sleepingThreads[i]].sleepCountdown == 0) {
 				threadHolder[sleepingThreads[i]].state = VM_THREAD_STATE_READY;
 				sleepingThreads.erase(sleepingThreads.begin()+i);
