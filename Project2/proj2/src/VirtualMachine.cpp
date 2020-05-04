2abcb5ec3e11741cb47ffca9b48e04d1809da130
gvazquez8 <gabvazquez@ucdavis.edu>
2020-05-04 03:12:04 -0700

    editing

--- Project2/proj2/src/VirtualMachine.cpp
+++ Project2/proj2/src/VirtualMachine.cpp
@@ -119,14 +119,14 @@ extern "C" {
 		if (entry == NULL || tid == NULL) {return VM_STATUS_ERROR_INVALID_PARAMETER;}
 
 		Thread *thread = new Thread();
-		thread.state = VM_THREAD_STATE_DEAD;
-		thread.entry = entry;
-		thread.args = param;
-		thread.memsize = memsize;
-		thread.prio = prio;
-		thread.stackaddr = malloc(thread.memsize * sizeof(TVMMemorySize));
+		thread->state = VM_THREAD_STATE_DEAD;
+		thread->entry = entry;
+		thread->args = param;
+		thread->memsize = memsize;
+		thread->prio = prio;
+		thread->stackaddr = malloc(thread->memsize * sizeof(TVMMemorySize));
 
-		threadHolder[thread.prio - 1].push_back(&thread);
+		threadHolder[thread->prio - 1].push_back(thread);
 
 
 
