cf1bd3a2d9eddfac61326eb60fe380ea6181281f
gvazquez8 <gabvazquez@ucdavis.edu>
2020-05-05 02:51:41 -0700

    editing

--- Project2/proj2/apps/preempt.c
+++ Project2/proj2/apps/preempt.c
@@ -1,4 +1,4 @@
-#include "VirtualMachine.h" 	 	    		
+#include "VirtualMachine.h"
 
 #ifndef NULL
 #define NULL    ((void *)0)
@@ -8,7 +8,7 @@ void VMThread(void *param){
     volatile int *Val = (int *)param;
     int RunTimeTicks;
     TVMTick CurrentTick, EndTick;
-    
+
     VMTickMS(&RunTimeTicks);
     RunTimeTicks = (5000 + RunTimeTicks - 1)/RunTimeTicks;
     VMTickCount(&CurrentTick);
@@ -27,14 +27,14 @@ void VMMain(int argc, char *argv[]){
     volatile int Val1 = 0, Val2 = 0;
     volatile int LocalVal1, LocalVal2;
     VMPrint("VMMain creating threads.\n");
-    VMThreadCreate(VMThread, (void *)&Val1, 0x100000, VM_THREAD_PRIORITY_LOW, &VMThreadID1);
-    VMThreadCreate(VMThread, (void *)&Val2, 0x100000, VM_THREAD_PRIORITY_LOW, &VMThreadID2);
+    VMThreadCreate(VMThread, (void *)&Val1, 0x1000000, VM_THREAD_PRIORITY_LOW, &VMThreadID1);
+    VMThreadCreate(VMThread, (void *)&Val2, 0x1000000, VM_THREAD_PRIORITY_LOW, &VMThreadID2);
     VMPrint("VMMain activating threads.\n");
     VMThreadActivate(VMThreadID1);
     VMThreadActivate(VMThreadID2);
     VMPrint("VMMain Waiting\n");
     do{
-        LocalVal1 = Val1; 
+        LocalVal1 = Val1;
         LocalVal2 = Val2;
         VMThreadState(VMThreadID1, &VMState1);
         VMThreadState(VMThreadID2, &VMState2);
