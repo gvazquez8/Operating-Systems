a10bb59b86d008bee1704599f120271e53344589
gvazquez8 <gabvazquez@ucdavis.edu>
2020-05-04 21:39:35 -0700

    editing

--- Project2/proj2/src/VirtualMachine.cpp
+++ Project2/proj2/src/VirtualMachine.cpp
@@ -77,23 +77,23 @@ extern "C" {
 
 	void schedule(int scheduleEqualPrio) {
 
-		// for (unsigned int i = 0; i < readyThreads.size(); i++) {
-		// 	switch(i) {
-		// 		case 0:	std::cout << "LOW THREADS: " << std::endl;
-		// 		break;
-		// 		case 1: std::cout << "NORMAL THREADS: " << std::endl;
-		// 		break;
-		// 		case 2: std::cout << "HIGH THREADS: " << std::endl;
-		// 		break;
-		// 		default: break;
-		// 	}
-		// 	for (unsigned int j = 0; j < readyThreads[i].size(); j++) {
-		// 		std::cout << "thread ID: " << readyThreads[i].front() << std::endl;
-		// 		TVMThreadID next = readyThreads[i].front();
-		// 		readyThreads[i].pop();
-		// 		readyThreads[i].push(next);
-		// 	}
-		// }
+		for (unsigned int i = 0; i < readyThreads.size(); i++) {
+			switch(i) {
+				case 0:	std::cout << "LOW THREADS: " << std::endl;
+				break;
+				case 1: std::cout << "NORMAL THREADS: " << std::endl;
+				break;
+				case 2: std::cout << "HIGH THREADS: " << std::endl;
+				break;
+				default: break;
+			}
+			for (unsigned int j = 0; j < readyThreads[i].size(); j++) {
+				std::cout << "thread ID: " << readyThreads[i].front() << std::endl;
+				TVMThreadID next = readyThreads[i].front();
+				readyThreads[i].pop();
+				readyThreads[i].push(next);
+			}
+		}
 
 		TVMThreadID nextThread;
 
