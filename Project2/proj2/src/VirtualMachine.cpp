7a7e635a639aa7e75be77ea9f34e187c98df1cc0
gvazquez8 <gabvazquez@ucdavis.edu>
2020-05-05 22:12:01 -0700

    editing

--- Project2/proj2/src/VirtualMachine.cpp
+++ Project2/proj2/src/VirtualMachine.cpp
@@ -168,12 +168,6 @@ extern "C" {
 
 	void timerCallback(void* calldata) {
 		MachineSuspendSignals(&signalState);
-		if (threadHolder.size() > 2) {
-			std::cout << "3rd THREAD STATE: " << threadHolder[2].state << std::endl;
-		}
-		if (threadHolder.size() > 3) {
-			std::cout << "4rd THREAD STATE: " << threadHolder[3].state << std::endl;
-		}
 		totalTickCount++;
 		for (unsigned int i = 0; i < sleepingThreads.size(); i++) {
 			if (threadHolder[sleepingThreads[i]].sleepCountdown == 0) {
