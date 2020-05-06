1ff5f5d629739de122e59b879ca4aec4353b89df
gvazquez8 <gabvazquez@ucdavis.edu>
2020-05-05 21:47:02 -0700

    editing

--- Project2/proj2/src/VirtualMachine.cpp
+++ Project2/proj2/src/VirtualMachine.cpp
@@ -179,8 +179,8 @@ extern "C" {
 		totalTickCount++;
 		for (unsigned int i = 0; i < sleepingThreads.size(); i++) {
 			if (threadHolder[sleepingThreads[i]].sleepCountdown == 0) {
-				std::cout << "IN TIMER CALLBACK" << std::endl;
-				std::cout << "THREAD " << sleepingThreads[i] << ": " << threadHolder[sleepingThreads[i]].state << " -> READY" << std::endl;
+				// std::cout << "IN TIMER CALLBACK" << std::endl;
+				// std::cout << "THREAD " << sleepingThreads[i] << ": " << threadHolder[sleepingThreads[i]].state << " -> READY" << std::endl;
 				threadHolder[sleepingThreads[i]].state = VM_THREAD_STATE_READY;
 				readyThreads[threadHolder[sleepingThreads[i]].prio -1].push(threadHolder[sleepingThreads[i]].id);
 				sleepingThreads.erase(sleepingThreads.begin()+i);
