1ce06ca8a26d14a92255e353231146f8b5118177
gvazquez8 <gabvazquez@ucdavis.edu>
2020-05-05 22:23:36 -0700

    editing

--- Project2/proj2/src/VirtualMachine.cpp
+++ Project2/proj2/src/VirtualMachine.cpp
@@ -435,8 +435,8 @@ extern "C" {
 			if (threadHolder[args->id].state > threadHolder[currThread].state) {
 				threadHolder[currThread].state = VM_THREAD_STATE_READY;
 				threadHolder[args->id].state = VM_THREAD_STATE_READY;
-				readyThreads[threadHolder[currThread].prio-1].push(threadHolder[currThread].id);
 				readyThreads[threadHolder[args->id].prio-1].push(args->id);
+				readyThreads[threadHolder[currThread].prio-1].push(threadHolder[currThread].id);
 				schedule(0);
 			}
 		}
