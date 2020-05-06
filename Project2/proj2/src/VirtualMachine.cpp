cbf3d41603d77b7ea2a36d6cd138b5f2bc1ec355
gvazquez8 <gabvazquez@ucdavis.edu>
2020-05-05 20:12:38 -0700

    editing

--- Project2/proj2/src/VirtualMachine.cpp
+++ Project2/proj2/src/VirtualMachine.cpp
@@ -423,6 +423,7 @@ extern "C" {
 			return;
 		} else {
 			if (threadHolder[args->id].state > threadHolder[currThread].state) {
+				std::cout << "Making thread " << currThread << "READY" << std::endl;
 				threadHolder[currThread].state = VM_THREAD_STATE_READY;
 				readyThreads[threadHolder[args->id].prio-1].push(args->id);
 				schedule(0);
